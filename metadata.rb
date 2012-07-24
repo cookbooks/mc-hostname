ddf818e94427ff7254cbbc63abecd86c11d8d9ed
Dan Buch <d.buch@modcloth.com>
2012-07-23 21:40:03 -0400

    Wherps.  Can't access `node` in this context.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,3 @@ license          "All rights reserved"
 description      "Installs/Configures a hostname"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.2.1"
-
-if node.platform == 'windows'
-  depends 'powershell'
-end
